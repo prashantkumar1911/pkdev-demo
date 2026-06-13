d017512d16321fb8a4f5b147e6a5fbd6cde47f3e
Prashant <prashant.kumar1911@gmail.com>
2026-06-13 07:23:28 +0530

    Intial Commit

---
+++ src/main/java/com/example/pkdev_demo/PkdevDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pkdev_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkdevDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkdevDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pkdev_demo/PkdevDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pkdev_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkdevDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
